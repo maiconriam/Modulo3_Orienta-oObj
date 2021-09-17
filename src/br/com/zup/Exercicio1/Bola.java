de428a52a3cecbb1d5baefd406fdda872ada8cc7
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-17 14:32:45 -0300

    Exercicio1 obj

---
+++ src/br/com/zup/Exercicio1/Bola.java
@@ -0,0 +1,18 @@
+package br.com.zup.Exercicio1;
+
+public class Bola {
+    String circunferencia;
+    String cor;
+    String marca;
+    double velocidade;
+
+    public Bola(){
+
+    }
+    public Bola(String circunferencia, String cor, String marca, double velocidade){
+        this.circunferencia = circunferencia;
+        this.cor = cor;
+        this.marca = marca;
+        this.velocidade = velocidade;
+    }
+}
---
+++ src/br/com/zup/Exercicio1/Main.java
@@ -0,0 +1,27 @@
+package br.com.zup.Exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Bola redonda = new Bola();
+
+        redonda.circunferencia = "Redonda";
+        redonda.cor = "Branca e Preta";
+        redonda.marca = "Nike";
+        redonda.velocidade = 9.99;
+
+        System.out.println("Sua bola é: " + redonda.circunferencia);
+        System.out.println("A cor da bola é: " + redonda.cor);
+        System.out.println("A marca da bola é: " + redonda.marca);
+        System.out.println("A velocidade que a bola atinge é: " + redonda.velocidade);
+        System.out.println();
+
+        Bola quadrada = new Bola("Quadrada", "vermelha", "Adidas", 10);
+
+        System.out.println("Sua bola é: " + quadrada.circunferencia);
+        System.out.println("A cor da bola é: " + quadrada.cor);
+        System.out.println("A marca da bola é: " + quadrada.marca);
+        System.out.println("A velocidade que a bola atinge é: " + quadrada.velocidade);
+    }
+
+
+}
