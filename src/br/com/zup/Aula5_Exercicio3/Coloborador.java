469f60ec1ea690f462584d37214726dd3c7d9bc9
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-26 12:30:23 -0300

    Completando o LevelUP

--- src/br/com/zup/Aula5_Exercicio3/Coloborador.java
+++ src/br/com/zup/Aula5_Exercicio3/Coloborador.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Coloborador extends Funcionario{
-    private List<Professor> professoresList = new ArrayList<>();
+    private List<Professor> professoresSupervisionados = new ArrayList<>();
 
     public Coloborador(String nome, String cpf, double numeroRegistro, String orgaoLotacao, double salario) {
         super(nome, cpf, numeroRegistro, orgaoLotacao, salario);
     }
 
     public void adcionarProfessor(Professor novoProfessor){
-        professoresList.add(novoProfessor);
+        if(professoresSupervisionados.size() <= 10){
+            professoresSupervisionados.add(novoProfessor);
+        }else{
+            System.out.println("Você ja tem 10 professores supervisionados.");
+        }
     }
+
 
     @Override
     public double aumentoSalario(){
