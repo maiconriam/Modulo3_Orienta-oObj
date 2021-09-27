daed9517b5e54a3604372dcd5f74781178d918a8
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-27 10:45:09 -0300

    Melhorando lista

--- src/br/com/zup/Aula5_Exercicio3/Coloborador.java
+++ src/br/com/zup/Aula5_Exercicio3/Coloborador.java
@@ -1,23 +1,51 @@
 package br.com.zup.Aula5_Exercicio3;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Coloborador extends Funcionario{
-    private List<Professor> professoresSupervisionados = new ArrayList<>();
+    private Professor[] professoresSupervisionados = new Professor[10];
+    private int contador = 0;
+    private int qtdProfessores;
 
     public Coloborador(String nome, String cpf, double numeroRegistro, String orgaoLotacao, double salario) {
         super(nome, cpf, numeroRegistro, orgaoLotacao, salario);
     }
 
-    public void adcionarProfessor(Professor novoProfessor){
-        if(professoresSupervisionados.size() <= 10){
-            professoresSupervisionados.add(novoProfessor);
-        }else{
-            System.out.println("Você ja tem 10 professores supervisionados.");
-        }
+    public Professor[] getProfessoresSupervisionados() {
+        return professoresSupervisionados;
     }
 
+    public void setProfessoresSupervisionados(Professor[] professoresSupervisionados) {
+        this.professoresSupervisionados = professoresSupervisionados;
+    }
+
+    public int getQtdProfessores() {
+        return qtdProfessores;
+    }
+
+    public void setQtdProfessores(int qtdProfessores) {
+        this.qtdProfessores = qtdProfessores;
+    }
+
+    public void adicionarProfessor(Professor professor){
+        professoresSupervisionados[contador] = professor;
+        contador ++;
+        qtdProfessores ++;
+    }
+
+    public void mostrarProfessores(){
+        for (int indice = 0; indice < contador; indice ++){
+            System.out.println("Nome: " + professoresSupervisionados[indice].getNome());
+            System.out.println("CPF: " + professoresSupervisionados[indice].getCpf());
+            System.out.println("Número do registro: " + professoresSupervisionados[indice].getNumeroRegistro());
+            System.out.println("Orgão de Lotoção: " + professoresSupervisionados[indice].getOrgaoLotacao());
+            System.out.println("Salário: " + professoresSupervisionados[indice].getSalario());
+            System.out.println("Nivel de graduação: " + professoresSupervisionados[indice].getNivelGraduacao());
+            System.out.println("Displina: " + professoresSupervisionados[indice].getDisciplina());
+            System.out.println("Quantidade de alunos: " + professoresSupervisionados[indice].getQtdAlunos());
+            System.out.println("Turmas: " + professoresSupervisionados[indice].getQtdTurmas());
+        }
+    }
 
     @Override
     public double aumentoSalario(){
